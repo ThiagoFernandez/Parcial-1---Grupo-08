d8a68ffe53919b8c5fb43fa0545d49f00276e71d
ThiagoFernandez <thiagofprocacci@gmail.com>
2026-03-29 21:26:13 -0300

    starting ts

--- src/listModule/SimpleLinkedList.java
+++ src/listModule/SimpleLinkedList.java
@@ -1,4 +1,34 @@
 package listModule;
 
-public class SimpleLinkedList <E>{
+public class SimpleLinkedList <E> implements SimpleList<E>{
+    private MyNode<E> root;
+    private MyNode<E> tail;
+    private int size;
+
+    public SimpleLinkedList() {
+        this.root = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    @Override
+    public boolean add(E element){
+        MyNode<E> newNode = new MyNode<>(element);
+        if (root == null){
+            root = newNode;
+            tail = newNode;
+        } else{
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        return true;
+    }
+
+    @Override
+    public void add(E element){
+        MyNode
+    }
+
 }
+
